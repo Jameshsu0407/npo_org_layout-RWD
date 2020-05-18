691e7286d56cc1a084f00230407dbb04924ebe3a
Jameshsu0407 <Jameshsu0407@gmail.com>
2020-05-18 11:20:09 +0800

    卡片輪播

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ $('#ads').slick({
     autoplay: true,
     autoplaySpeed: 2000,
 });
+
+$('#activity_card').slick({
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:true,
+    // centerMode: true,
+    arrows: false
+});
