f678f2e767c0f2cd78d31acf9c4e669807f1f1e8
Jameshsu0407 <Jameshsu0407@gmail.com>
2020-08-06 21:27:35 +0800

    longPressDelay: 25,

--- js/script.js
+++ js/script.js
@@ -208,11 +208,11 @@ document.addEventListener('DOMContentLoaded', function () {
         showNonCurrentDates: false,
         fixedWeekCount: true,
         selectable: true,
-        longPressDelay: 50,
+        longPressDelay: 25,
         contentHeight: 450,
         dateClick: function(date) {
             window.location.href = "1.html";
-            alert("longPressDelay: 50,");
+            alert("longPressDelay: 25,");
         },
         // select: function(startDate, endDate) {
         //     window.location.href = "#";
